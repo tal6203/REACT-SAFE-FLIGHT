e90ee7516a3d43d426a881298b5fc2f3c39b1b10
Tal Abutbul <112417918+tal6203@users.noreply.github.com>
2024-06-18 13:27:36 +0300

    Update CountriesController.java

--- Server/src/main/java/com/example/demo/controllers/CountriesController.java
+++ Server/src/main/java/com/example/demo/controllers/CountriesController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin
+@CrossOrigin(origins = "https://safe-flight.netlify.app")
 @RestController
 @RequestMapping("/api/countries")
 public class CountriesController {
